ea1ca3d939361dafd38428ee7c9b2d19f190c88f
s-martis <marti.samore@gmail.com>
2024-08-12 17:58:18 +0200

    Correzione errori

--- myapp/myapp/src/main/java/com/example/myapp/MapDBConfig.java
+++ myapp/myapp/src/main/java/com/example/myapp/MapDBConfig.java
@@ -1,11 +1,11 @@
 package com.example.myapp;
 
+import javax.annotation.PreDestroy;
+
 import org.mapdb.DB;
 import org.mapdb.DBMaker;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import javax.annotation.PreDestroy;
 
 @Configuration
 public class MapDBConfig {
@@ -15,14 +15,22 @@ public class MapDBConfig {
     @Bean
     public DB mapDB() {
         // Configurazione del database MapDB su file
-        db = DBMaker.fileDB("userdb.db").make();
+        db = DBMaker.fileDB("userdb.db")
+                .checksumHeaderBypass()  // Bypass del controllo dell'intestazione
+                .transactionEnable()     // Abilita le transazioni
+                .make();
         return db;
     }
 
     @PreDestroy
     public void closeDB() {
         if (db != null) {
-            db.close();
+            try {
+                db.close();
+                System.out.println("Database chiuso con successo.");
+            } catch (Exception e) {
+                System.err.println("Errore durante la chiusura del database:");
+            }
         }
     }
 }
